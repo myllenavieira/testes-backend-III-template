b1fd203439560696cd3d1de33e732adaf7bb7410
myllenavieira <vieirasilvamyllena@gmail.com>
2023-06-12 20:24:17 -0300

    :test_tube: test: Criando novos testes

--- tests/business/login.test.ts
+++ tests/business/login.test.ts
@@ -6,6 +6,7 @@ import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
 import { UserDatabaseMock } from "../mocks/UserDatabaseMock"
 import { NotFoundError } from "../../src/errors/NotFoundError"
+import { describe, expect, test } from '@jest/globals';
 
 describe("Testando login", () => {
   const userBusiness = new UserBusiness(
@@ -14,7 +15,43 @@ describe("Testando login", () => {
     new TokenManagerMock(),
     new HashManagerMock()
   )
+  test("deve retornar erro caso email não seja string", () => {
+    expect.assertions(1)
+    try {
+      const input = LoginSchema.parse({
+        email: 5,
+        password: "fulano123"
+      })
+    } catch (error) {
+      if (error instanceof ZodError) {
+        expect(error.issues).toEqual([
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'number',
+            path: ['email'],
+            message: 'Expected string, received number'
+          }
+        ])
+      }
+    }
+  })
+  test("deve retornar erro caso email não esteja cadastrado", async () => {
+    expect.assertions(2)
+    try {
+      const input = LoginSchema.parse({
+        email: "vieiramyllena@email.com",
+        password: "fulano123"
+      })
 
+      await userBusiness.login(input)
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        expect(error.message).toBe("'email' não encontrado")
+        expect(error.statusCode).toBe(404)
+      }
+    }
+  })
   test("deve gerar um token ao logar", async () => {
     const input = LoginSchema.parse({
       email: "fulano@email.com",
